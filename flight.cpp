379a31db3c8063de364ddd128d808cc04a49d727
hanahassan <hana0hassan0@gmail.com>
2023-12-03 18:17:14 -0700

    final

--- flight.cpp
+++ flight.cpp
@@ -93,7 +93,7 @@ void Flight::remove_passenger(int passengerID) {
  // Function to create an empty seat map
 void Flight::create_empty_seat_map() {
     // Ensure rows and columns are non-negative
-    if (num_rowsM <= 0 || num_columnsM <= 0) {
+    if (num_rowsM < 0 || num_columnsM < 0) {
         cerr << "Invalid number of rows or columns. Please set valid values.\n";
         return;
     }
@@ -112,7 +112,7 @@ void Flight::create_empty_seat_map() {
 // Function to resize the seat map
 void Flight::resize_seat_map(int rows, int columns) {
     // Ensure rows and columns are non-negative
-    if (rows <= 0 || columns <= 0) {
+    if (rows < 0 || columns < 0) {
         cerr << "Invalid number of rows or columns. Please set valid values.\n";
         return;
     }
